1bc2bfad0f03e5a44f13a738a46b4b82946dec58
TheGodGAMER <129994492+JoseMiguelPerusquiaHernandez@users.noreply.github.com>
2024-06-27 00:16:51 -0600

    ACTUALIZACION DE PYHTON

--- Mates_II/app.py
+++ Mates_II/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import sympy as sp
 
 app = Flask(__name__)
 
@@ -14,10 +15,19 @@ def enfriamiento():
 def transformadas():
     return render_template('Transformadas.html')
 
-
-
-
-
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    try:
+        data = request.json
+        funcion = data['funcion']
+        t = sp.symbols('t')
+        f_t = sp.sympify(funcion)
+        s = sp.symbols('s')
+        F_s = sp.laplace_transform(f_t, t, s, noconds=True)
+        latex_result = sp.latex(F_s)
+        return jsonify({'result': latex_result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
